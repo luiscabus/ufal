df6c1b563aa5484c2b4006e6d5750b4e3adc3ba2
Luis Alberto Cabús <luiscabus@gmail.com>
2018-08-01 20:47:47 -0300

    commit from windows

--- rascunho.c
+++ rascunho.c
@@ -1,2 +1,51 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+
+void map(int (*f)(int), int array[], int limite, int i) {
+	if (i == limite) {
+		return;
+	}
+
+	array[i] = (*f)(array[i]);
+
+	map((*f), array, limite, ++i);
+}
+
+int square(int x) {
+	return x * x;
+}
+
+int cube(int x) {
+	return x * x * x;
+}
+
+int main() {
+
+	int array[4];
+	array[0] = 1;
+	array[1] = 2;
+	array[2] = 3;
+	array[3] = 4;
+
+	map(square, array, 4, 0);
+
+	int i;
+
+	for (i = 0; i < 4; ++i)
+	{
+		printf("Pos %d Val %d\n", i, array[i]);
+	}
+	
+	map(cube, array, 4, 0);
+	
+	for (i = 0; i < 4; ++i)
+	{
+		printf("Pos %d Val %d\n", i, array[i]);
+	}
+	
+
+	return 0;
+}
